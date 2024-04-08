d027e568268361e8d7bfbcee5ceaef4917ffded1
C-Dresser <142123153+C-Dresser@users.noreply.github.com>
2024-04-07 21:25:26 -0400

    added functionality to download my resume and began reformatting links on the portfolio page

--- portfolio/src/pages/Portfolio.jsx
+++ portfolio/src/pages/Portfolio.jsx
@@ -1,3 +1,6 @@
+import React from 'react';
+import Project from '../components/project.jsx';
+
 export default function Portfolio() {
 
   const styles = {
@@ -9,6 +12,12 @@ export default function Portfolio() {
       borderLeft: '1px solid #efefef',
       borderColor: '#6272a4',
     },
+    projectGrid: {
+      display: 'grid',
+      justifyContent: 'center',
+      gridTemplateColumns: 'repeat(3, 1fr)',
+      gap: '20px',
+    },
   };
 
   return (
@@ -17,26 +26,38 @@ export default function Portfolio() {
       <p style={styles.innerContainer}>
         Here are some of the projects I have worked on:
       </p>
-      <ui>
-        <li>
-          <a href="https://c-dresser.github.io/password-generator/" target="_blank" rel="noopener noreferrer" style={styles.link}>Password Generator</a>
-        </li>
-        <li>
-          <a href="https://c-dresser.github.io/coding-quiz/" target="_blank" rel="noopener noreferrer" style={styles.link}>Multiple Choice Quiz</a>
-        </li>
-        <li>
-          <a href="https://c-dresser.github.io/work-day-scheduler/" target="_blank" rel="noopener noreferrer" style={styles.link}>Workday Scheduler</a>
-        </li>
-        <li>
-          <a href="https://rachaelkstokes.github.io/trailblazerz/" target="_blank" rel="noopener noreferrer" style={styles.link}>Trailblazerz</a>
-        </li>
-        <li>
-          <a href="https://github.com/C-Dresser/express-note-taker/blob/main/readme.md" target="_blank" rel="noopener noreferrer" style={styles.link}>Express Note Taker</a>
-        </li>
-        <li>
-          <a href="https://github.com/C-Dresser/sql-employee-tracker/blob/main/README.md" target="_blank" rel="noopener noreferrer" style={styles.link}>SQL Employee Tracker</a>
-        </li>
-      </ui>
+      <div style={styles.projectGrid}>
+      <Project
+        imageUrl="https://raw.githubusercontent.com/C-Dresser/password-generator/main/Assets/Images/Password-Generator-image.png"
+        title="Password Generator"
+        link="https://c-dresser.github.io/password-generator/"
+      />
+      <Project
+        imageUrl="https://raw.githubusercontent.com/C-Dresser/coding-quiz/main/Assets/Images/quiz-question-screen.png"
+        title="Multiple Choice Quiz"
+        link="https://c-dresser.github.io/coding-quiz/"
+      />
+      <Project
+        imageUrl="https://raw.githubusercontent.com/C-Dresser/work-day-scheduler/main/Assets/Work-Day-Scheduler-schedule-screen.png"
+        title="Workday Scheduler"
+        link="https://c-dresser.github.io/work-day-scheduler/"
+      />
+      <Project
+        imageUrl="https://raw.githubusercontent.com/C-Dresser/weather-dashboard/main/assets/weather-dashboard-screenshot.png"
+        title="Weather Dashboard"
+        link="https://c-dresser.github.io/weather-dashboard/"
+      />
+      <Project
+        imageUrl="https://c-dresser.github.io/express-note-taker/assets/images/express-note-taker.png"
+        title="Express Note Taker"
+        link="https://c-dresser.github.io/express-note-taker/"
+      />
+      <Project
+        imageUrl="https://c-dresser.github.io/sql-employee-tracker/assets/images/sql-employee-tracker.png"
+        title="SQL Employee Tracker"
+        link="https://github.com/C-Dresser/sql-employee-tracker/blob/main/README.md"
+      />
+      </div>
     </div>
   );
 }
--- portfolio/src/pages/Resume.jsx
+++ portfolio/src/pages/Resume.jsx
@@ -1,3 +1,5 @@
+import React from 'react';
+
 export default function Resume() {
 
     const styles = {
@@ -17,6 +19,9 @@ export default function Resume() {
           <p style={styles.innerContainer}>
           My qualifications include a certificate in Full Stack Web Development and experience with HTML, CSS, JavaScript, Express, SQL, Handlebars, React, Node.js, and MongoDB. Take a look at my resume:
         </p>
+        <a href="https://raw.githubusercontent.com/C-Dresser/living-resume/main/curtisDresserResume2024.pdf" download="your_pdf_filename.pdf">
+        Download PDF
+      </a>
       </div>
     );
   }
